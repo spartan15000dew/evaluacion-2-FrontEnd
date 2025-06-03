d54dd0a2144a009f4732541cabd1722cf0337cfc
spartan15000dew <spartan15000dew@gmail.com>
2025-06-03 11:30:34 -0400

    comit final

--- script3.js
+++ script3.js
@@ -1,4 +1,24 @@
 //telefono debe tener exactamente 8 digitos, no es obligatoria
 //contrasenia debe tener mas de 5 caracteres, es obligatoria
 
-let telefonos = []
+telefonos = []
+
+function validar(){
+let Etelefono = document.getElementById("telefono")
+Vtelefono = Etelefono.
+let errorTelefono= document.getElementById("errorTelefono")
+
+let Econtrasena = document.getElementById("password")
+Vcontrasena = Econtrasena.
+let errorContrasena= document.getElementById("errorPassword")
+
+if{}
+
+
+
+
+
+
+
+}
+
